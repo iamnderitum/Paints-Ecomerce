470c19805ce437c83c97327b10ed77d1cc3e2488
Moses Ndungu <moseswambui044@gmail.com>
2024-10-29 02:54:30 +0300

    Added Django Backend $ Proper Migrations

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from products.models import Category,Product
 
+@admin.register(Category)
 # Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", )
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "category", "price")
--- products/models.py
+++ products/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, null=True, blank=True)
+    code = models.CharField(max_length=50, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50, null=True)
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        null=True
+    )
+    price = models.CharField(max_length=50, null=True)
+    description = models.TextField(null=True)
