af3e940d8df95ee986fdc887dc152c101e6d865a
Moses Ndungu <moseswambui044@gmail.com>
2024-11-01 20:56:31 +0300

    Include Template and CSS Files

--- products/models.py
+++ products/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=50, null=True, blank=True)
+    
     code = models.CharField(max_length=50, null=True, blank=True)
 
     def __str__(self):
