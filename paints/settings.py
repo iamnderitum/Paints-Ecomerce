d96ba580f1d1fb0c8beb754de960339c2cccda8d
Moses Ndungu <moseswambui044@gmail.com>
2024-11-08 02:50:36 +0300

    Settings

--- paints/settings.py
+++ paints/settings.py
@@ -89,12 +89,12 @@ WSGI_APPLICATION = 'paints.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.paints',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 # DATABASES = {
 #     'default': dj_database_url.config(
 #         # Replace this value with your local database's connection string.
@@ -102,8 +102,10 @@ WSGI_APPLICATION = 'paints.wsgi.application'
 #         conn_max_age=600
 #     )
 # }
+
+# Render PostgreSQL configuration for production
 if not DEBUG:
-    "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    DATABASES["default"] = dj_database_url.config(default=os.getenv("DATABASE_URL"))
 
 else:
     DATABASES = {
