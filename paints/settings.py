516bf57972317e835d2a7934edc70342c7e96653
Moses Ndungu <moseswambui044@gmail.com>
2024-11-08 20:02:59 +0300

    Changed DB URI

--- paints/settings.py
+++ paints/settings.py
@@ -87,16 +87,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'paints.wsgi.application'
 
+DATABASES = {
+    'default': dj_database_url.config(default="postgresql://paint:4t5IUWLuiszDIeiOSqcf9vAmzGRYmWEF@dpg-csmksd5umphs73ak16u0-a/paints_kni0")
+}
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.paints',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.paints',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 # DATABASES = {
 #     'default': dj_database_url.config(
 #         # Replace this value with your local database's connection string.
@@ -106,20 +109,20 @@ DATABASES = {
 # }
 
 # Render PostgreSQL configuration for production
-if not DEBUG:
-    DATABASES["default"] = dj_database_url.config(default="postgresql://paint:4t5IUWLuiszDIeiOSqcf9vAmzGRYmWEF@dpg-csmksd5umphs73ak16u0-a/paints_kni0")
+# if not DEBUG:
+#     DATABASES["default"] = dj_database_url.config(default="postgresql://paint:4t5IUWLuiszDIeiOSqcf9vAmzGRYmWEF@dpg-csmksd5umphs73ak16u0-a/paints_kni0")
 
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'paints',
-            'USER': 'paint',
-            'PASSWORD': '011235813',
-            'HOST': 'localhost',
-            'PORT': '5432',
-        }
-    }
+# else:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.postgresql',
+#             'NAME': 'paints',
+#             'USER': 'paint',
+#             'PASSWORD': '011235813',
+#             'HOST': 'localhost',
+#             'PORT': '5432',
+#         }
+#     }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
