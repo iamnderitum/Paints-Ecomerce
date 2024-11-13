3feb653aa893ac4814078484320e2eb830a0113d
Moses Ndungu <moseswambui044@gmail.com>
2024-11-13 19:45:26 +0300

    Enable static files

--- paints/settings.py
+++ paints/settings.py
@@ -176,4 +176,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-#STATIC_URL = '/static/'
+STATIC_URL = '/static/'
